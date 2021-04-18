7742367cd06f51cb20e271325a94d5453f4aed16
KennedyQ <80281153+KennedyQ@users.noreply.github.com>
2021-04-18 14:05:42 -0400

    Slight changes to follow new input convention

--- generatefourvoicepiece.py
+++ generatefourvoicepiece.py
@@ -7,10 +7,10 @@ if (__name__ == "__main__"):
 	notefilename = "pythoninput.txt"
 	csoundfilename = "pythontest.txt"
 	generator = CSound.ProceduralOrchestra()
-	soprano = CSound.Performer(60,79,.45,.45,1.05,72,False,[2,2,1,2,2,2,1],False)
-	alto = CSound.Performer(55,72,.1,.1,2,60,True,[2,2,1,2,2,2,1],True,1)
-	tenor = CSound.Performer(48,67,.1,.1,2,60,True,[2,2,1,2,2,2,1],True,1)
-	bass = CSound.Performer(41,60,.1,.1,2,48,True,[2,2,1,2,2,2,1],True,1)
+	soprano = CSound.Performer(60,79,.45,.45,1.05,72,False,"Major",False)
+	alto = CSound.Performer(55,72,.1,.1,2,60,False,"Major",True,1)
+	tenor = CSound.Performer(48,67,.1,.1,2,60,False,"Major",True,1)
+	bass = CSound.Performer(41,60,.1,.1,2,48,False,"Major",True,1)
 	orchestra = [soprano, alto, tenor, bass]
 	generator.generateNoteString(notefilename, 120, 240, orchestra)
 	generator.printFormat(csoundfilename, notefilename, orchestra)
